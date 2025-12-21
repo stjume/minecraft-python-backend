3520e83c2d871e7464500b59ab723f94170edc82
Adrian Oeyen <oeyenadrian@gmail.com>
2025-12-21 14:52:06 +0100

    refactor PostChat to new ArgumentParser

--- src/main/java/org/sk/skMinecraft/commands/PostChat.java
+++ src/main/java/org/sk/skMinecraft/commands/PostChat.java
@@ -1,14 +1,17 @@
 package org.sk.skMinecraft.commands;
 
+import java.util.Arrays;
+
 import org.bukkit.Bukkit;
 import org.sk.skMinecraft.SkMinecraft;
+import org.sk.skMinecraft.SkMinecraft.StringCommand;
 
 public class PostChat extends Command {
 
     private final String message;
 
-    public PostChat(String command) {
-        this.message = command.replaceFirst("postChat" + SkMinecraft.seperator, "");
+    public PostChat(StringCommand command) {
+        this.message = Arrays.stream(command.arguments()).toString();
     }
 
     @Override
