ab980b9ed91bd8fbe215c451e442d43d7b3518e6
Adrian Oeyen <oeyenadrian@gmail.com>
2025-12-21 17:57:28 +0100

    fix bug in postChat

--- src/main/java/org/sk/skMinecraft/commands/PostChat.java
+++ src/main/java/org/sk/skMinecraft/commands/PostChat.java
@@ -1,6 +1,7 @@
 package org.sk.skMinecraft.commands;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
 import org.sk.skMinecraft.SkMinecraft.StringCommand;
@@ -10,7 +11,7 @@ public class PostChat extends Command {
     private final String message;
 
     public PostChat(StringCommand command) {
-        this.message = Arrays.stream(command.arguments()).toString();
+        this.message = Arrays.stream(command.arguments()).collect(Collectors.joining(""));
     }
 
     @Override
